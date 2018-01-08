2a990a29c3a7699170235fd742ecdf2535276f7d
ikbenlike <thewatcheriswatchinu@gmail.com>
2018-01-08 17:17:39 +0100

    adds testing code for image mechanism

--- src/main.c
+++ src/main.c
@@ -6,8 +6,22 @@
 int main(int argc, char **argv){
     svm_t *vm = svm_init(100, 100, 100, 0);
     size_t sptr = 0;
-    svm_append_opcode(vm->code, sptr++, SIN);
+    svm_append_opcode(vm->code, sptr++, PUSH);
+    svm_append_bool(vm->code, sptr++, true);
+    svm_append_opcode(vm->code, sptr++, BPRINT);
+    svm_append_opcode(vm->code, sptr++, PUSH);
+    svm_append_integer(vm->code, sptr++, 10);
+    svm_append_opcode(vm->code, sptr++, IPRINT);
+    svm_append_opcode(vm->code, sptr++, PUSH);
+    svm_append_float(vm->code, sptr++, 10.345);
+    svm_append_opcode(vm->code, sptr++, FPRINT);
+    svm_append_opcode(vm->code, sptr++, PUSH);
+    svm_append_char(vm->code, sptr++, 'd');
+    svm_append_opcode(vm->code, sptr++, CPRINT);
+    svm_append_opcode(vm->code, sptr++, PUSH);
+    svm_append_string(vm->code, sptr++, svm_string_from_cstr("Hello, Image World!"));
     svm_append_opcode(vm->code, sptr++, SPRINT);
+    //svm_append_opcode(vm->code, sptr++, SPRINT);
     /*svm_append_opcode(vm->code, sptr++, PUSH);
     svm_append_string(vm->code, sptr++, svm_string_from_cstr("Hello, Function World!"));
     svm_append_opcode(vm->code, sptr++, SPRINT);
